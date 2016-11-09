0bfc9b4b9235c493374cea26dbcb38e25a36ec96
TanoCuile <strifinder@gmail.com>
2016-11-09 23:15:07 +0200

    Fix db

--- app/config/db.php
+++ app/config/db.php
@@ -9,12 +9,12 @@ use Silex\Application;
 // DB config
 if ($app) {
     $app['db'] = function (Application $app) {
-        $connection = new mysqli($app['db_host'], $app['db_user'], $app['db_password'], $app['db_database']);
-
-        Service\DatabaseUtils::$prefix = $app['db_prefix'];
-        Service\DatabaseUtils::setDrupalDB($connection);
-
-        $connection->connect_errno && die("Database error");
+//        $connection = new mysqli($app['db_host'], $app['db_user'], $app['db_password'], $app['db_database']);
+//
+//        Service\DatabaseUtils::$prefix = $app['db_prefix'];
+//        Service\DatabaseUtils::setDrupalDB($connection);
+//
+//        $connection->connect_errno && die("Database error");
         return $connection;
     };
     // Load service
